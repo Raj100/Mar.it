e73c484929bae8d73b79fdcf0a98405149c0675b
Raj Rawariya <98628762+Raj100@users.noreply.github.com>
2024-04-19 02:01:38 +0530

    .

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="flex gap-2">
+      <input className="border p-2 shadow" type="text" name="" id="" />
+      <div className="bg-blue-500 p-2 w-10 rounded-lg"><i></i></div>
+      </div>
+    </main>
+  );
+}
